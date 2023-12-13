156f2f405e613c936cae699c459997af6c93efd5
ibrahima <ibrahima.diallo1289@gmail.com>
2023-12-13 01:12:47 -0500

    destroy backend

--- backend/providers.tf
+++ backend/providers.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+  
+  backend "s3" {
+    bucket     	   = "ibrahim-website.com"
+    key        	   = "state"
+    region         = "us-east-1"
+    encrypt    	   = false # Should be set to true
+    # dynamodb_table = "dev_tf_lockid"
+  }
 }
 
 provider "aws" {
