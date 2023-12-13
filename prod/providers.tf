05b97de1c8fa32fb8852b3d663598232c500e6f7
ibrahima <ibrahima.diallo1289@gmail.com>
2023-12-13 00:40:57 -0500

    adding new environments and workflows

---
+++ prod/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source       = "hashicorp/aws"
+      version      = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+   
+  backend "s3" {
+    bucket     	   = "prod-tfstate2023"
+    key        	   = "state/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt    	   = false # Should be set to true
+    # dynamodb_table = "dev_tf_lockid"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
