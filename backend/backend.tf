f76c113cf802ab36de183968a13562b5bd264a2d
ibrahima <ibrahima.diallo1289@gmail.com>
2023-12-14 23:06:10 -0500

    adding manual pproval

--- backend/backend.tf
+++ backend/backend.tf
@@ -54,3 +54,13 @@ resource "aws_s3_bucket_policy" "terraform_aws_state" {
   }
   EOF
 }
+
+# Remove public access block
+resource "aws_s3_bucket_public_access_block" "terraform_aws_state" {
+  bucket = aws_s3_bucket.terraform_aws_state.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  depends_on = [ aws_s3_bucket.terraform_aws_state ]
+}
